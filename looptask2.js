241060027aa230f9b3b97e86d0db03d99b742076
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-31 23:03:10 +0600

    Complete js while loop task 2 find odd number and even number

---
+++ looptask2.js
@@ -0,0 +1,26 @@
+//-------------Subtask-1---------------------
+
+let num = 61;
+
+while(num <= 100){
+    if(num % 2  === 1){
+        console.log('odd numbers', num);
+    }
+    console.log(num);
+    num++;
+    
+}
+
+
+
+//---------------Subtask-2--------------------
+
+let num2 = 78;
+
+while(num2 <= 98){
+    if(num2 % 2 === 0){
+        console.log('even number:', num2);
+    }
+    console.log(num2);
+    num2++;
+}
