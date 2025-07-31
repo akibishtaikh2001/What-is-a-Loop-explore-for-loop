fc6770bef0235f69843ebffb177230306e8b5164
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-31 19:47:04 +0600

    While loop

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if (number < 10){
+    console.log('choto eakta number paisi');
+}
+console.log('something new is cooking');
+
+//---------WHILE--LOOP____________
+let num = 0; // loop variable
+while(num < 5){
+    console.log('looooooping');
+    // num = num + 1;
+    // num+= 1;
+    num++;
+}
