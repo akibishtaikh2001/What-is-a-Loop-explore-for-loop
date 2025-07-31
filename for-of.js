0645496518a0e8160c4311799c62a958b2f8b18f
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-31 18:19:37 +0600

    for-of loop

---
+++ for-of.js
@@ -0,0 +1,20 @@
+// const numbers = [12, 98, 45, 54, 81, 19];
+
+// console.log(numbers);
+
+// for (const num of numbers) {
+//     console.log(num);
+// }
+
+
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits) {
+    console.log(fruit);
+    console.log('I want to eat');
+}
